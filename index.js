8a6a5b9d45e101e7d39ece6d7bde2e410f7369f2
erivaldo junior <erivaldo.junior@referenceit.com.br>
2023-03-17 13:32:57 -0300

    args no puppeteer

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ axios
 botStart = function () {
   const client = new Client({
     authStrategy: new LocalAuth(),
-    puppeteer: { headless: false },
+    puppeteer: { headless: false ,args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-accelerated-2d-canvas', '--no-first-run', '--no-zygote', '--single-process', '--disable-gpu'] },
   });
 
   client.initialize();
